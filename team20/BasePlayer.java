18b3599d6dc286ffb91e702c1b63382532cf438a
Lucas Taubert <ltaubert@csc.kth.se>
2012-11-10 12:48:02 +0100

    Lol

--- team20/BasePlayer.java
+++ team20/BasePlayer.java
@@ -1,28 +1,49 @@
 package team20;
 
-import hockey.api.Position;
 import hockey.api.Player;
+import hockey.api.Position;
+
+import java.awt.Color;
+import java.util.Random;
 
 public abstract class BasePlayer extends Player {
-    // The middle of the opponents goal, on the goal line
-    protected static final Position GOAL_POSITION = new Position(2600, 0);
+	// The middle of the opponents goal, on the goal line
+	protected static final Position GOAL_POSITION = new Position(2600, 0);
+	private Random rnd;
 
-    // Left handed?
-    public boolean isLeftHanded() { return false; }
+	// Left handed?
+	public boolean isLeftHanded() {
+		return false;
+	}
 
-    // Initiate
-    public void init() {
-    }
+	// Initiate
+	public void init() {
+		rnd = new Random();
+	}
 
-    // Face off
-    public void faceOff() {
-    }
+	// Face off
+	public void faceOff() {
+	}
 
-    // Penalty shot
-    public void penaltyShot() {
-    }
+	// Penalty shot
+	public void penaltyShot() {
+		int targetX = 3000 - 400;
+		int targetY = rnd.nextInt() % 180 - 90;
 
-    // Player intelligence goes here
-    public void step() {
-    }
+		setDebugPoint(GOAL_POSITION.getX(), GOAL_POSITION.getY(), Color.BLUE);
+		
+		boolean shoot = false;
+
+		if (getX() > 1000)
+			shoot = true;
+		if (shoot) {
+			shoot(GOAL_POSITION, MAX_SHOT_SPEED);
+		} else {
+			skate(GOAL_POSITION, MAX_SPEED);
+		}
+	}
+
+	// Player intelligence goes here
+	public void step() {
+	}
 }
--- team20/Center.java
+++ team20/Center.java
@@ -1,5 +1,8 @@
 package team20;
 
+import hockey.Util;
+import hockey.api.IPlayer;
+
 public class Center extends BasePlayer {
     // Number of center player
     public int getNumber() { return 19; }
@@ -9,8 +12,18 @@ public class Center extends BasePlayer {
 
     // Center player's intelligence
     public void step() {
-	if (hasPuck())
-	    skate(GOAL_POSITION, MAX_SPEED);
+	if (hasPuck()){
+		// Distance to enemy goal keeper is less than 1k.
+		IPlayer self = getPlayer(5);
+		IPlayer goalie = getGoalKeeper(6);
+		double goalDist = Math.sqrt(Math.pow(self.getX() - goalie.getX(),2) + Math.pow(self.getY() - goalie.getY(), 2));
+		if(goalDist < 1000){
+			penaltyShot();
+		}
+		else{
+			skate(GOAL_POSITION, MAX_SPEED);
+		}
+	}
 	else
 	    skate(0, getPuck().getY(), 1000);
     }
