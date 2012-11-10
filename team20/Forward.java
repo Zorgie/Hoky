4add95b20fbf05047530871c71d720bfe6e08c00
otchii <otchii@zenbook>
2012-11-10 14:18:37 +0100

    Stuff

--- team20/Forward.java
+++ team20/Forward.java
@@ -4,13 +4,16 @@ import hockey.api.Position;
 
 public class Forward extends BasePlayer {
 	private Position forwardPoint;
+	private Position forwardReturn;
 	// Number of forward
 	
 	public Forward(int which){
 		if(which==0){
-			forwardPoint = new Position(2600, 500);
-		}else{
 			forwardPoint = new Position(2600, -500);
+			forwardReturn = new Position(0, -500);
+		}else{
+			forwardPoint = new Position(2600, 500);
+			forwardReturn = new Position(2600, 500);
 		}
 	}
 	
@@ -34,6 +37,19 @@ public class Forward extends BasePlayer {
 		if (hasPuck()) {
 			shoot(getPlayer(5), 4444); // pass center player
 		} else {
+			if (this.getHeading() > -90 && this.getHeading() <=90){
+				if(getX() > 1750){
+					skate(forwardReturn, MAX_SPEED/2);
+					endStep();
+					return;
+				}
+			}else{
+				if(getX() <1250){
+					skate(forwardPoint, MAX_SPEED/2);
+					endStep();
+					return;
+				}
+			}
 			skate(forwardPoint, MAX_SPEED); // get the puck
 		}
 
